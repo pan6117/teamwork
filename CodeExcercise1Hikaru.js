8806f77ffdb0158d66271984c6992cad11c8ebf6
Hikaru Miyata <m.shirokumakingdom@gmail.com>
2022-09-26 19:47:08 -0700

    first commit

---
+++ CodeExcercise1Hikaru.js
@@ -0,0 +1,63 @@
+// //Question1
+// let num1 = 10
+// let num2 = 20
+
+// function printLargerNum (){
+//     if (num1>num2) {
+//         console.log(num1,'is larger')
+//     }
+//     else if (num1<num2){
+//         console.log(num2, 'is larger')
+//     }
+//     else 
+//         console.log(num1, 'and', num2, 'are the same')
+// };
+//  printLargerNum ();
+
+
+//Question 2
+//Using a for loop print all even numbers up to and including n. Don’t include 0.
+
+// let n = 30;
+
+// for (let i = 1;i<=n; i++){
+//     if(i%2==0){
+//     console.log (i+'is even number')
+ 
+// }
+// }
+
+
+
+/*Question3 Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.*/
+
+// let strings = "Yeah";
+// function printYes () {
+//     if(strings.includes('Y')){
+//         console.log("yes")
+//     }
+//     else {console.log("no")}
+// };
+// printYes()
+
+
+/*Write a function which accepts 4 parameters (different scores of a student for subjects), and
+then calculate the average of all the score.*/
+
+// function gradeAverage (score1,score2,score3,score4)  {
+//     let average = (score1+score2+score3+score4) 
+//     if (90<=average) {
+//         console.log('Grade A')
+//     }
+//     else if (70<=average<90) {
+//         console.log('Grade B')
+//     }
+//     else if (50<=average<70) {
+//         console.log('Grade C')
+//     } 
+//     else {
+//         console.log('Grade F')
+//     }
+// };
+
+// gradeAverage(70,80,90,90);
---
+++ arraysMoviesHomework.js
@@ -0,0 +1,33 @@
+let movies = [
+    {
+        name: "Avengers",
+        releaseDate: 2018,
+        director: "Joe Russo",
+        actor: "Robert Browney Jr"
+    },
+    {
+    name: "Captain America",
+    releaseDate: 2016,
+    director: "Anthony Russo",
+    actor: "Tom Holland"
+    },
+    {
+    name: "Homecoming",
+        releaseDate: 2017,
+        director: "Jon Watts",
+        actor: "Hikaru Miyata"
+    },
+    {
+    name: "Home alone",
+        releaseDate: 1999,
+        director: "Chris Columbus ",
+        actor: "Macaulay Culkin"
+    },
+    {
+    name: "Child's Play",
+        releaseDate: 1988,
+        director: "Tom Holland",
+        actor: "Don Mancini"
+    }
+]
+console.log(movies)
